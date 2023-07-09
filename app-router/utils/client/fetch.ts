1a5ee74660e67605e56e675023bd51292bbf83b7
Jarrett Meyer <jarrettmeyer@gmail.com>
2023-07-09 06:53:14 -0400

    switch to using axios

--- app-router/utils/client/fetch.ts
+++ app-router/utils/client/fetch.ts
@@ -1,15 +1,10 @@
+import axios from "axios";
+
 export async function fetchApiDbData<TResponse = unknown, TRequest = unknown>(
   action: string,
   request?: TRequest,
 ): Promise<TResponse> {
   const url = `/api/db?action=${action}`;
-  const body = JSON.stringify(request || null);
-  const method = "POST";
-  const init: RequestInit = { body, method };
-  const response = await fetch(url, init);
-  if (response.ok) {
-    const json = await response.json();
-    return json;
-  }
-  throw new Error(`Failed to fetch ${url}, status: ${response.status}, body: ${await response.text()}`);
+  const response = await axios.post(url, request || {});
+  return response.data as TResponse;
 }
