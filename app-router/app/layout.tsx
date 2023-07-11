e3eea8f38e500ab90c0cc66748fe28f97576e297
Jarrett Meyer <jarrettmeyer@gmail.com>
2023-07-10 20:48:22 -0400

    adds Container component

--- app-router/app/layout.tsx
+++ app-router/app/layout.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/Container";
 import "@/styles/globals.css";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -10,34 +11,36 @@ export default function Layout({ children }: PropsWithChildren) {
     <html lang="en">
       <body className="min-h-screen w-full flex flex-col justify-stretch items-stretch">
         <header className="w-full bg-black text-white">
-          <nav className="container mx-auto py-4 flex flex-row gap-6 justify-stretch items-center">
-            <h1 className="block text-xl font-semibold mb-0">Learn NextJS &mdash; App Router</h1>
-            <div className="flex-grow flex flex-row gap-2">
-              <div>
-                <Link href="/" className="text-white">
-                  Home
-                </Link>
+          <Container>
+            <nav className="py-4 flex flex-row gap-6 justify-stretch items-center">
+              <h1 className="block text-xl font-semibold mb-0">Learn NextJS &mdash; App Router</h1>
+              <div className="flex-grow flex flex-row gap-2">
+                <div>
+                  <Link href="/" className="text-white">
+                    Home
+                  </Link>
+                </div>
+                <div>
+                  <Link href="/tasks" className="text-white">
+                    Tasks
+                  </Link>
+                </div>
               </div>
               <div>
-                <Link href="/tasks" className="text-white">
-                  Tasks
+                <Link href="/profile" className="text-white">
+                  Guest
                 </Link>
               </div>
-            </div>
-            <div>
-              <Link href="/profile" className="text-white">
-                Guest
-              </Link>
-            </div>
-          </nav>
+            </nav>
+          </Container>
         </header>
         <main className="flex-grow py-8">
-          <div className="container mx-auto">{children}</div>
+          <Container>{children}</Container>
         </main>
         <footer className="bg-gray-200 shadow text-gray-700 text-sm">
-          <div className="container mx-auto py-8">
+          <Container className="py-8">
             <p className="mb-0">&copy; {new Date().getFullYear()}</p>
-          </div>
+          </Container>
         </footer>
       </body>
     </html>
